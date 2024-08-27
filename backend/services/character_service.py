1fac60b42db5763886dc73c5da2118bb59144f71
caprice_dev <108645246+caprice1026-disc@users.noreply.github.com>
2024-08-27 19:28:44 +0900

    キャラクター作成部分に少し追加

---
+++ backend/services/character_service.py
@@ -0,0 +1,15 @@
+from flask_restx import Resource
+from models import db, Player
+
+class CharacterAPI(Resource):
+    def setcharacter(self):
+        # キャラクター情報を取得するロジックを追加する予定
+        pass
+
+    def CreateCharacter(self, name, character_class, back_story):
+        # キャラクターを作成するロジックを追加する予定
+        #新しいPlayerインスタンスを作成（Playerクラスはmodels.pyで定義、各値は引数で渡す）
+        new_player = Player(name=name, character_class=character_class, back_story=back_story)
+        #新しいPlayerインスタンスをデータベースに追加
+        db.session.add(new_player)
+        pass
